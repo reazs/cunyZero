494deb0a12606a5231026e0c3ea0fc8b4e7bfa71
reazs <reaz.shakil@onegoalgraduation.org>
2021-12-05 11:05:06 -0500

    added access to instructor page

--- CUNYZero/cunyzero/forms.py
+++ CUNYZero/cunyzero/forms.py
@@ -63,4 +63,7 @@ class CreateClassForm(FlaskForm):
     time = SelectField("Class Meeting Time", validators=[DataRequired()], choices=[
         "9:00AM-10:30Am", "11:00AM-12:30PM", "2:00PM-3:45PM", "6:00PM-7:45PM"
     ])
+    changeTerm = SelectField("Change Terms", validators=[DataRequired()], choices=[
+         "set-up","Register", "running", "grading"
+    ])
     submit = SubmitField("Submit")
